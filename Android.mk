7ae8399719bb2f048e6c41b9dd09a74a185fe949
Mark M <mark@localhost>
2012-10-12 16:41:32 +0800

    Delete Old Stuff

--- Android.mk
+++
@@ -1,4 +0,0 @@
-
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),c8812)
-include $(call all-named-subdir-makefiles, libcamera liblights libaudio libril )
-endif
--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-# Best hack ever.
-cp device/huawei/c8812/path/HWQcomRIL.java frameworks/base/telephony/java/com/android/internal/telephony/
-add_lunch_combo huawei_c8812-eng
-add_lunch_combo huawei_c8812-userdebug
